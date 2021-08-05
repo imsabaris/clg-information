a7064ce4636121d228428d6379d66751edac6e49
imsabaris <imsabaris@gmail.com>
2021-08-05 15:50:15 +0530

    saba

--- src/org/college/Student.java
+++ src/org/college/Student.java
@@ -6,6 +6,7 @@ public class Student extends College {
 	}
 	public void studentDept() {
 		System.out.println("mech");
+		System.out.println("Yes possible ");
 	}
 	public void studentId() {
 		System.out.println("12ece123");
